b028ae90826caf3607bc49e6211dcb9cea721626
id2k1149 <id2k1149@gmail.com>
2022-10-09 15:44:56 -0400

    Initial Commit

---
+++ SwiftBookFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookFirstApp
+//
+//  Created by Max Franz Immelmann on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
